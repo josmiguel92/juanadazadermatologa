eb137704e83984ce5dc3b15ee48e7915cd4c6549
Josue Miguel <josmiguel92@gmail.com>
2022-10-03 12:47:30 -0400

    adding some text

--- components/features/EspecialidadesHome.vue
+++ components/features/EspecialidadesHome.vue
@@ -3,14 +3,17 @@
     <div class="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
       <h2 class="max-w-lg mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
         <span class="inline-block mb-1 sm:mb-4">
-          The quick, brown fox<br class="hidden md:block" />
-          jumps over a lazy dog
+          Nos preocupamos por ti!
+
         </span>
         <div class="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100"></div>
       </h2>
       <p class="text-gray-700 lg:text-sm lg:max-w-md">
-        "Sed ut perspiciatis unde omnis iste natus error sit iste voluptatem accusantium doloremque rem aperiam, ipsa eaque quae. Sed ut perspiciatis unde omnis iste."
+        Este sitio web te ayudara a entender de forma clara las enfermedades de la piel, uñas y cabello, ademas de aclararte dudas de procedimiento esteticos y medicos, es decir que hacer antes, durante y despues de estos.
+        <br class="hidden md:block" />
+        <span class="font-bold">te brindaremos la mejor asesoria con mucho amor!</span>
       </p>
+
     </div>
     <div class="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
       <a  v-for="(item, index) in featuresItems"
@@ -32,14 +35,14 @@
       </a>
 
     </div>
-    <div class="text-center">
+    <!-- <div class="text-center">
       <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
         See more
         <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
           <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
         </svg>
       </a>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
