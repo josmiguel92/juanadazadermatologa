f89a7856c6c20bde1814951c22d1581c4b1aa329
josmiguel92 <josmiguel92@users.noreply.github.com>
2022-10-10 03:51:52 +0000

    deploy: ff93d07ae69845339987d2f42f093e677f6b7778

--- _nuxt/static/1665371034/especialidades/dermatologia-adultos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665371034",layout:"default",error:a,serverRendered:true,routePath:"\u002Fespecialidades\u002Fdermatologia-adultos",config:{BASE_URL:b,_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
